cc52ba3a1c3de4864cac0d2db4bc35c79e25ec69
Elena Solomko <lena06051981be@gmail.com>
2022-11-28 15:45:00 +0200

    Update TransactionHistory.jsx

---
+++ src/components/Transaction/TransactionHistory.jsx
@@ -0,0 +1,18 @@
+export const TransactionHistory =({}) => {
+    return (
+        <div>
+            <thead>
+                <tr>
+                    <th>Type</th>
+                    <th>Amount</th>
+                    <th>Currency</th>
+                </tr>
+            </thead>
+            
+            
+
+        </div>
+
+
+    )
+}
