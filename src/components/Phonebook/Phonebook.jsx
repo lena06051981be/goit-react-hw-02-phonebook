ec73a7f6c0c306eca978da1c3da5cedb1c01ff63
Elena Solomko <lena06051981be@gmail.com>
2022-12-05 17:49:10 +0200

    Update Phonebook.jsx

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -13,19 +13,32 @@ class Phonebook extends Component {
 
     render() {
         return (
-        <div>
+            <div style={{ display: 'flex', flexDirection: 'column', width: '320px' }}>
             <h2>Phonebook Test</h2>
+            <label>Name</label>
             <input
                 type="text"
                 name="name"
-                    value={this.state.inputValue}
+                value={this.state.inputValue}
                 onChange={this.handleInputChange}
 
                 pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                 title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                 required
             />
-            <button>Add contact</button>
+            <label>Number</label>
+            <input                
+                type="text"
+                name="name"
+                value={this.state.inputValue}
+                onChange={this.handleInputChange}
+
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+            />
+            
+            <button type="submit">Add contact</button>
         </div>
     )
     }
